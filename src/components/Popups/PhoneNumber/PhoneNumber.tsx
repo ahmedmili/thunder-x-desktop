7457101c3c344dbb3004f259f057952f5d61587c
mili ahmed <ahmedmili76@gmail.com>
2023-09-15 13:56:14 +0100

    phone popup

---
+++ src/components/Popups/PhoneNumber/PhoneNumber.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+import './phoneNumber.scss'
+
+import LocalPhoneRoundedIcon from '@mui/icons-material/LocalPhoneRounded';
+interface Props {
+    close: any
+}
+
+const PhoneNumber: React.FC<Props> = ({ close }) => {
+
+    return (
+        <div className="popup-container" onClick={close}>
+            <div className="popup-box">
+                <LocalPhoneRoundedIcon className="phone-icon" />
+                <span className="phone-number">44 100 162</span>
+            </div>
+        </div>
+    )
+
+}
+
+export default PhoneNumber
--- src/components/layout/Profile/Discuter/Discuter.tsx
+++ src/components/layout/Profile/Discuter/Discuter.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './discuter.scss'
 import Cover from '../../../../assets/profile/Discuter/cover.png'
 import Service from '../../../../assets/profile/Discuter/Service-24-7.png'
+import PhoneNumber from '../../../Popups/PhoneNumber/PhoneNumber'
 function Discuter() {
+    const [phonePopup, setPhonePopup] = useState<boolean>(false)
+    const handlePhonePopup = () => {
+        setPhonePopup(!phonePopup)
+    }
     return (
         <div className='discuter-main-container'>
             <header className='discuter-header'>
@@ -15,7 +20,7 @@ function Discuter() {
                     <h2 className='title'>Besoin d’aide ?</h2>
                     <p className='sub-title'>Contactez le support client</p>
                     <div className='buttons'>
-                        <button className='btn1'>J’ai quelque chose à dire</button>
+                        <button className='btn1' onClick={handlePhonePopup}>J’ai quelque chose à dire</button>
                         <button className='support-client'>Contactez le support client</button>
                     </div>
                 </div>
@@ -23,10 +28,12 @@ function Discuter() {
                     <h2 className='title'>Service clientel</h2>
                     <p className='sub-title'>Disponible 24/7</p>
                     <div className='service-24' style={{ backgroundImage: ` url(${Service})` }}>
-
                     </div>
                 </div>
             </main>
+            {
+                phonePopup && <PhoneNumber close={handlePhonePopup} />
+            }
         </div >
     )
 }
