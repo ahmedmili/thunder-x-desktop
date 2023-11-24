571e353ed9d24dec127d84d291bfea80c5bee454
mili ahmed <ahmedmili76@gmail.com>
2023-11-24 09:49:24 +0100

    routing fixes

--- src/components/layout/Profile/ConfigPage/Legale/Legale.tsx
+++ src/components/layout/Profile/ConfigPage/Legale/Legale.tsx
@@ -7,7 +7,6 @@ import './legale.scss';
 import legalsData from './legals.json';
 
 import { useTranslation } from 'react-i18next';
-import { useAppSelector } from '../../../../../Redux/store';
 
 interface legalProps {
   title: string,
@@ -36,20 +35,12 @@ const Legale = () => {
   const [totalPages, setTotalPages] = useState<number>(0);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [displayedContent, setDisplayedContent] = useState<any[]>([]);
-
-  const theme = useAppSelector((state) => state.home.theme)
-  const [template, setTemplate] = useState<number>(theme)
-
   const handleContent = () => {
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
     const displayedContent = legalsData.slice(startIndex, endIndex)
     setDisplayedContent(displayedContent)
   }
-
-  useEffect(() => {
-    setTemplate(theme)
-  }, [theme])
 
   useEffect(() => {
     handleContent()
@@ -72,46 +63,44 @@ const Legale = () => {
 
 
   return (
-    <>
-      <section className="Lgale-section">
-        <h1 style={{ color: `${template === 1 ? "white" : "black"}` }}>{t('profile.mesConfig.legales.message')}</h1>
-        <div className="logo-container">
+    <section className="Lgale-section">
+      <h1>{t('profile.mesConfig.legales.message')}</h1>
+      <div className="logo-container">
 
-          <div className="logo-wrapper" style={{ backgroundImage: `url(${thunderLogo})` }}>
+        <div className="logo-wrapper" style={{ backgroundImage: `url(${thunderLogo})` }}>
+        </div>
+      </div>
+      <main>
+        {
+          (displayedContent && displayedContent.length > 0) && (
+            displayedContent.map((legal: any, index: number) => {
+              return <LegalCart key={index} title={legal.title} bodyText={legal.body} />
+            })
+          )
+
+        }
+      </main>
+      <div className='buttons'>
+        {/* prev button */}
+        {!(currentPage === 1) &&
+          <div className="nav-page-button">
+            <button onClick={prevPage}>
+              <img loading="lazy" src={LeftArrow} alt="prev button" />
+            </button>
           </div>
-        </div>
-        <main>
-          {
-            (displayedContent && displayedContent.length > 0) && (
-              displayedContent.map((legal: any, index: number) => {
-                return <LegalCart key={index} title={legal.title} bodyText={legal.body} />
-              })
-            )
+        }
+        {/* next */}
+        {!(currentPage === totalPages) &&
+          <div className="nav-page-button">
+            <button onClick={nextPage}>
+              <img loading="lazy" src={RigthArrow} alt="prev button" />
 
-          }
-        </main>
-        <div className='buttons'>
-          {/* prev button */}
-          {!(currentPage === 1) &&
-            <div className="nav-page-button">
-              <button onClick={prevPage}>
-                <img loading="lazy" src={LeftArrow} alt="prev button" />
-              </button>
-            </div>
-          }
-          {/* next */}
-          {!(currentPage === totalPages) &&
-            <div className="nav-page-button">
-              <button onClick={nextPage}>
-                <img loading="lazy" src={RigthArrow} alt="prev button" />
+            </button>
+          </div>
+        }
+      </div>
+    </section >
 
-              </button>
-            </div>
-          }
-        </div>
-      </section >
-
-    </>
   );
 };
 
--- src/components/layout/Profile/ProfileSideBar/SideBar.tsx
+++ src/components/layout/Profile/ProfileSideBar/SideBar.tsx
@@ -1,34 +1,35 @@
-import './sideBar.scss'
 import { useEffect, useState } from 'react';
-import { useAppDispatch, useAppSelector } from '../../../../Redux/store';
-import { logout } from '../../../../Redux/slices/userSlice';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 import { setProfilePage } from '../../../../Redux/slices/home';
+import { logout } from '../../../../Redux/slices/userSlice';
+import { useAppDispatch, useAppSelector } from '../../../../Redux/store';
 import { localStorageService } from '../../../../services/localStorageService';
-import { Link, useLocation } from 'react-router-dom';
+import './sideBar.scss';
 
-import profileImg from '../../../../assets/profile/profile_img.png'
-import phoneIcon from "../../../../assets/profile/phoneIcon.svg"
-import lockIcon from "../../../../assets/profile/lockIcon.svg"
+import lockIcon from "../../../../assets/profile/lockIcon.svg";
+import phoneIcon from "../../../../assets/profile/phoneIcon.svg";
+import profileImg from '../../../../assets/profile/profile_img.png';
 
-import Accueil from '../../../../assets/profile/blue/Accueil.svg'
-import Offres from '../../../../assets/profile/blue/Offres.svg'
-import Config from '../../../../assets/profile/blue/Config.svg'
-import Archive from '../../../../assets/profile/blue/Archive.svg'
-import Espace from '../../../../assets/profile/blue/Espace.svg'
-import Discuter from '../../../../assets/profile/blue/Discuter.svg'
-import Favors from '../../../../assets/profile/blue/Favors.svg'
-import Deconnecter from '../../../../assets/profile/blue/Deconnecter.svg'
+import Accueil from '../../../../assets/profile/blue/Accueil.svg';
+import Archive from '../../../../assets/profile/blue/Archive.svg';
+import Config from '../../../../assets/profile/blue/Config.svg';
+import Deconnecter from '../../../../assets/profile/blue/Deconnecter.svg';
+import Discuter from '../../../../assets/profile/blue/Discuter.svg';
+import Espace from '../../../../assets/profile/blue/Espace.svg';
+import Favors from '../../../../assets/profile/blue/Favors.svg';
+import Offres from '../../../../assets/profile/blue/Offres.svg';
 
-import AccueilW from '../../../../assets/profile/white/Accueil.svg'
-import OffresW from '../../../../assets/profile/white/Offres.svg'
-import ConfigW from '../../../../assets/profile/white/Config.svg'
-import ArchiveW from '../../../../assets/profile/white/Archive.svg'
-import EspaceW from '../../../../assets/profile/white/Espace.svg'
-import DiscuterW from '../../../../assets/profile/white/Discuter.svg'
-import FavorsW from '../../../../assets/profile/white/Favors.svg'
-import { LocationService } from '../../../../services/api/Location.api';
 import { toast } from 'react-toastify';
+import AccueilW from '../../../../assets/profile/white/Accueil.svg';
+import ArchiveW from '../../../../assets/profile/white/Archive.svg';
+import ConfigW from '../../../../assets/profile/white/Config.svg';
+import DiscuterW from '../../../../assets/profile/white/Discuter.svg';
+import EspaceW from '../../../../assets/profile/white/Espace.svg';
+import FavorsW from '../../../../assets/profile/white/Favors.svg';
+import OffresW from '../../../../assets/profile/white/Offres.svg';
+import { LocationService } from '../../../../services/api/Location.api';
+import { scrollToTop } from '../../../../utils/utils';
 
 
 const SideBar = () => {
@@ -47,11 +48,11 @@ const SideBar = () => {
 
 
   // save path on refresh
-  const pathSchema = ["/", "/profile/annonces", "/profile", "/profile/archivedCommands", "/profile/Fidelite", "/profile/discuter", "/profile/Favors"]
+  const pathSchema = ["/", "/profile/annonces/", "/profile/", "/profile/archivedCommands/", "/profile/Fidelite/", "/profile/discuter/", "/profile/Favors/"]
   useEffect(() => {
     const location = window.location.pathname
-    console.log("win location = ", location)
     setSelectedNav((pathSchema.indexOf(location) + 1))
+    scrollToTop()
   }, [currentPage])
 
   const handleSelect = (e: any, index: number) => {
@@ -100,37 +101,37 @@ const SideBar = () => {
             </li>
 
             <li>
-              <Link to={'/profile/annonces'} className={selectedNav == 2 ? "active" : ""} onClick={(e) => handleSelect(e, 2)}>
+              <Link to={'/profile/annonces/'} className={selectedNav == 2 ? "active" : ""} onClick={(e) => handleSelect(e, 2)}>
                 <span className='profile-list-icon' style={{ backgroundImage: `url(${selectedNav === 2 ? OffresW : Offres})` }}></span>
                 {t('profile.mesOffres')}
               </Link>
             </li>
             <li>
-              <Link to={'/profile'} className={selectedNav == 3 ? "active" : ""} onClick={(e) => handleSelect(e, 3)}>
+              <Link to={'/profile/'} className={selectedNav == 3 ? "active" : ""} onClick={(e) => handleSelect(e, 3)}>
                 <span className='profile-list-icon' style={{ backgroundImage: `url(${selectedNav === 3 ? ConfigW : Config})` }}></span>
                 {t('profile.mesConfig')}
               </Link>
             </li>
             <li>
-              <Link to={'/profile/archivedCommands'} className={selectedNav == 4 ? "active" : ""} onClick={(e) => handleSelect(e, 4)}>
+              <Link to={'/profile/archivedCommands/'} className={selectedNav == 4 ? "active" : ""} onClick={(e) => handleSelect(e, 4)}>
                 <span className='profile-list-icon' style={{ backgroundImage: `url(${selectedNav === 4 ? ArchiveW : Archive})` }}></span>
                 {t('profile.commands')}
               </Link>
             </li>
             <li>
-              <Link to={'/profile/Fidelite'} className={selectedNav == 5 ? "active" : ""} onClick={(e) => handleSelect(e, 5)}>
+              <Link to={'/profile/Fidelite/'} className={selectedNav == 5 ? "active" : ""} onClick={(e) => handleSelect(e, 5)}>
                 <span className='profile-list-icon' style={{ backgroundImage: `url(${selectedNav === 5 ? EspaceW : Espace})` }}></span>
                 {t('profile.espaceFidel')}
               </Link>
             </li>
             <li>
-              <Link to={'/profile/discuter'} className={selectedNav == 6 ? "active" : ""} onClick={(e) => handleSelect(e, 6)}>
+              <Link to={'/profile/discuter/'} className={selectedNav == 6 ? "active" : ""} onClick={(e) => handleSelect(e, 6)}>
                 <span className='profile-list-icon' style={{ backgroundImage: `url(${selectedNav === 6 ? DiscuterW : Discuter})` }}></span>
                 {t('profile.discuter')}
               </Link>
             </li>
             <li>
-              <Link to={'/profile/Favors'} className={selectedNav == 7 ? "active" : ""} onClick={(e) => handleSelect(e, 7)}>
+              <Link to={'/profile/Favors/'} className={selectedNav == 7 ? "active" : ""} onClick={(e) => handleSelect(e, 7)}>
                 <span className='profile-list-icon' style={{ backgroundImage: `url(${selectedNav === 7 ? FavorsW : Favors})` }}></span>
                 {t('profile.favors')}
               </Link>
