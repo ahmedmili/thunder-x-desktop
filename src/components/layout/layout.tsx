b5961afe86b952b2e67071917c27e4cbeacfe64d
amsetti04 <setti.amal.dev@gmail.com>
2023-11-27 09:40:06 +0100

    menu mobile

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,14 +1,32 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../footer/footer';
-
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import MobileMenuContent from '../mobile-menu-content/MobileMenuContent';
 const Layout = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const { t } = useTranslation();
+  const donwloadApp = () => {
+    window.open("https://play.google.com/store/apps/details?id=io.thunder.express.tn&pli=1", "_blank");
+  };
 
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
   return (
     <div>
-      <Header />
-      <Outlet />
-      <Footer />
+      <div className={`desktop-menu ${isMobileMenuOpen ? "menu-open" : ""}`}>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+      <MobileMenuContent
+        isMobileMenuOpen={isMobileMenuOpen}
+        toggleMobileMenu={toggleMobileMenu}
+        donwloadApp={donwloadApp}
+      />
     </div>
   );
 };
