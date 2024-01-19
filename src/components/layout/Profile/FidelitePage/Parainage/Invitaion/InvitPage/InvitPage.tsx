75b8a559de91995548a90c32344dc41b32328d03
Ghassen Hassine <gh.hsine@gmail.com>
2024-01-19 14:45:47 +0100

    THD Fidelite Page

--- src/components/layout/Profile/FidelitePage/Parainage/Invitaion/InvitPage/InvitPage.tsx
+++ src/components/layout/Profile/FidelitePage/Parainage/Invitaion/InvitPage/InvitPage.tsx
@@ -4,7 +4,7 @@ import './invitPagee.scss';
 import copy from 'clipboard-copy';
 import { useEffect, useState } from 'react';
 import MoneyPack from '../../../../../../../assets/profile/MoneyPack.png';
-import chaine from '../../../../../../../assets/profile/chaine.svg';
+import chaine from '../../../../../../../assets/profile/chaine-white.svg';
 import SocialShare from '../../../../../../../assets/profile/socialShare.svg';
 import { userService } from '../../../../../../../services/api/user.api';
 import { localStorageService } from '../../../../../../../services/localStorageService';
@@ -50,42 +50,61 @@ function InvitPage({ internNavigation }: InvitPageProps) {
     return (
         <main className='invit-page-container'>
             <h3 className='P-title' onClick={() => internNavigation(1)} ><ArrowBackIcon className='go-back-icon'></ArrowBackIcon> {t('profile.fidelite.Parrainage')}</h3>
-            <section className='money-image-container'>
-                <div className='money-img' style={{ backgroundImage: `url(${MoneyPack})` }} > </div>
-            </section>
-            {
-                (code && sponserShipeActivated) ?
-                    (
-                        <section className='copy-code-section' >
-                            <h2 className='invite-Q'>{t('profile.fidelite.inviteFriends')} ?</h2>
-                            <div className='code-input-container'>
-                                <input type="text" maxLength={6} readOnly value={code} />
-                                <button onClick={copyCode} className='copy-btn' ><span className='chaine-icon' style={{ backgroundImage: `url(${chaine})` }} ></span> {t('Copier')}</button>
-                            </div>
-                        </section>
-                    )
-                    : (
-                        <>
-                            <p className='sponsorship-not-activated-message'>
-                                {t('profile.fidelite.sponsorShipNotDisponible')}
-                            </p>
-                        </>
-                    )
-            }
+            <div className="parrainage-wrapper">
+                {/* 
+                <section className='money-image-container'>
+                    <div className='money-img' style={{ backgroundImage: `url(${MoneyPack})` }} > </div>
+                </section>
+                */}
 
-            <section className='notes-section'>
-                <h2>{t('profile.fidelite.inviteStart.howTo.title')}</h2>
-                <p>{t('profile.fidelite.inviteStart.howTo.description')}</p>
-            </section>
+                <div className='notes-section'>
+                    <h2>{t('profile.fidelite.inviteStart.howTo.title')}</h2>
+                    <p>{t('profile.fidelite.inviteStart.howTo.description')}</p>
+                </div>
 
-            <button onClick={handleModal} className='social-share-button'>
-                <span className='social-share-icon' style={{ backgroundImage: `url(${SocialShare})` }}></span>
-                {t('profile.fidelite.inviteNow')}
-            </button>
 
-            {
-                OpenModal && <ShareSocialModal code={code} close={handleModal} />
-            }
+                <div className="invite-friends-area">
+                    <h3 className='invite-Q'>{t('profile.fidelite.inviteFriends')} ?</h3>
+                    <div className="invite-friends-wrapper">
+                        <div className="invite-friends-blc">
+                            {
+                                (code && sponserShipeActivated) ?
+                                    (
+                                        <div className='copy-code-section' >
+                                            <div className='code-input-container'>
+                                                <input type="text" maxLength={6} readOnly value={code} />
+                                                <button onClick={copyCode} className='copy-btn' ><span className='chaine-icon' style={{ backgroundImage: `url(${chaine})` }} ></span> {t('Copier')}</button>
+                                            </div>
+                                        </div>
+                                    )
+                                    : (
+                                        <>
+                                            <p className='sponsorship-not-activated-message'>
+                                                {t('profile.fidelite.sponsorShipNotDisponible')}
+                                            </p>
+                                        </>
+                                    )
+                            }
+                            
+                        </div>
+                        <div className="invite-friends-blc">
+                            <button onClick={handleModal} className='social-share-button'>
+                                <span className='social-share-icon' style={{ backgroundImage: `url(${SocialShare})` }}></span>
+                                {t('profile.fidelite.inviteNow')}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                
+
+                
+
+                
+
+                {
+                    OpenModal && <ShareSocialModal code={code} close={handleModal} />
+                }
+            </div>
         </main>
 
     )
