b34c0faf31f0630d2c72acb44f950e69aae2e06b
Ghassen Hassine <gh.hsine@gmail.com>
2024-01-19 10:34:04 +0100

    THD Favoris

--- src/components/layout/Profile/Favors/Favors.tsx
+++ src/components/layout/Profile/Favors/Favors.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import './favors.scss'
 import Cover from '../../../../assets/profile/Discuter/cover.png'
+import favorisCover from '../../../../assets/favoris-bg.png'
 import { userService } from '../../../../services/api/user.api'
 import SupplierCard from '../../../supplierCard/SupplierCard'
 import { Restaurant } from '../../../../services/types'
@@ -18,20 +19,33 @@ function Favors() {
         <>
             <div className='favors-main-container'>
                 <header className='favors-header'>
-                    <div className='cover-img' style={{ backgroundImage: `url(${Cover})` }} >
-                    </div>
+                    {/* 
+                        <div className='cover-img' style={{ backgroundImage: `url(${Cover})` }} >
+                        </div>
+                    */}
+                    <img src={favorisCover} alt="favoris Cover" />
                 </header >
                 <main className='favors-main'>
-                    {
-                        favorsList.map((favor: Restaurant, index: number) => {
-
-                            return (
-                                <div className='supplier-card-grid-container'>
-                                    <SupplierCard className={'favors-supplier-card'} favors={true} data={favor} key={index} />
-                                </div>
-                            )
-                        })
-                    }
+                    <div className="favors-main_title-blc">
+                        <h1 className="favors-main_title">Votre resto préféré !</h1>
+                    </div>
+                    <div className="favors-wrapper">
+                        <p className="favors-list-desc">
+                            Ajouté récemment
+                        </p>
+                        <div className="favors-list-area">
+                            {
+                                favorsList.map((favor: Restaurant, index: number) => {
+                                    return (
+                                        <div className='supplier-card-grid-container'>
+                                            <SupplierCard className={'favors-supplier-card'} favors={true} data={favor} key={index} />
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                    
                 </main>
             </div >
         </>
